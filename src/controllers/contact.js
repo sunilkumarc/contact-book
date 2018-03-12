fc1a52d71768770e56e09c07423a122d8b8670f9
Sunil Kumar C <sunilkumar.c682@gmail.com>
2018-03-12 23:54:25 +0530

    Pagination implemented.

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -1,16 +1,81 @@
 var Contact = require('../models/contact');
 
 module.exports.set = (app) => {
-    app.get('/contact/:email', (req, res) => {
-        Contact.findOne({email: req.params.email}).then((contact) => {
-            console.log(contact);
-            if (contact) {
-                res.status(200).send(contact);
-            } else {
-                res.status(404).send('No contact found with this email');
+    app.get('/contact/', (req, res) => {
+        if (!req.query.email && !req.query.name) {
+            return res.status(500).send({"message": "Either email or name parameter should be sent in the url"});
+        }
+        if (req.query.email) {
+            Contact.findOne({email: req.query.email}).then((contact) => {
+                if (contact) {
+                    return res.status(200).send(contact);
+                } else {
+                    return res.status(404).send('No contact found with this email');
+                }
+            }).catch((err) => {
+                return res.status(400).send(err);
+            });
+        } else if (req.query.name) {
+            var perPage = 2;
+            var page = req.query.page || 1;
+            
+            // Contact.find({name: new RegExp('^' + req.query.name, 'i')}).then((contacts) => {
+            //     if (contacts) {
+            //         return res.status(200).send(contacts);
+            //     }
+            //     return res.status(404).send('No contacts found with this name');
+            // }).catch((err) => {
+            //     return res.status(500).send({"message": err});
+            // });
+            Contact.find({name: new RegExp('^' + req.query.name, 'i')})
+            .skip((perPage * page) - perPage)
+            .limit(perPage)
+            .then((contacts) => {
+                if (contacts) {
+                    return res.status(200).send(contacts);
+                }
+                return res.status(404).send('No contacts found with this name');
+            })
+            .catch((err) => {
+                return res.status(500).send({"message": err});
+            });
+        }
+    });
+
+    app.delete('/contact/:email', (req, res) => {
+        Contact.findOneAndRemove({email: req.params.email}, (err, contact) => {
+            if (err) {
+                return res.status(500).send(err);
             }
-        }).catch((err) => {
-            res.status(400).send(err);
+            if (!contact) {
+                return res.status(200).send({"message":"Email does not exists."});
+            }
+            const response = {
+                message: "Successfully Deleted",
+                contact: contact
+            };
+            return res.status(200).send(response);
+        });
+    });
+
+    app.put('/contact/', (req, res) => {
+        var contact = new Contact(req.body);
+        contact = contact.toObject();
+        delete contact._id;
+
+        if (!contact.email) {
+            return res.status(500).send({"message": "Email id is missing"});
+        }
+        Contact.update({email: contact.email}, contact, (err, updated) => {
+            if (err) {
+                return res.status(500).send(err);
+            } else {
+                const message = {
+                    message: "Updated Successfully.",
+                    contact: contact
+                }
+                return res.status(200).send(message);
+            }
         });
     });
 
@@ -18,9 +83,9 @@ module.exports.set = (app) => {
         var contact = new Contact(req.body);
 
         contact.save(req.body).then((contact) => {
-            res.status(200).send(contact);
+            return res.status(200).send(contact);
         }).catch((err) => {
-            res.status(400).send(err);
+            return res.status(400).send(err);
         })
     });
 }
