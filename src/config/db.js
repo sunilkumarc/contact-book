f3457c4eb9330dcb8ae89e77a2f2c729d2919575
Sunil Kumar C <sunilkumar.c682@gmail.com>
2018-03-14 20:49:31 +0530

    Fixed environment variables

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const db_url = process.env.MONGO_URI || 'mongodb://localhost:27017/plivo-contact-book';
 
+console.log('HERE', db_url);
 mongoose.connect(db_url, function(err, conn) {
     if(err) {
         console.log('Error while connecting to Mongoose : ' + err);
